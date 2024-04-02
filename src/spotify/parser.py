ba82f6bd01420aa34181e12cb7e4cc5765030822
Walid <walidoo_2002@hotmail.com>
2024-04-02 19:24:37 -0400

    enhance parser with middleware support for detailed object modifications

--- src/spotify/parser.py
+++ src/spotify/parser.py
@@ -61,14 +61,37 @@ class SpotifyParser:
             self.__mapping = json.load(f)
             self.__mapper = JsonToObjectMapper(self.__mapping)
 
-    def parse_all(self):
+    def parse_all(self, middleware=None):
         """
         Parses all JSON files in the `self.__items_folder_path` directory, converting each into
-        an instance of `self.__cls`.
+        an instance of `self.__cls`. An optional middleware function can be provided to modify
+        each object after it is mapped from the JSON data.
+
+        The middleware function should take two arguments: the mapped object and the original JSON data,
+        and it should return the modified object. If no middleware is provided, objects are returned as mapped
+        without any additional modifications.
+
+        Parameters:
+            middleware (Callable[[T, Dict], T]): A function that takes the mapped object and the original
+                                                 JSON data, modifies the mapped object, and returns it.
 
         Returns:
             A list of instances of `self.__cls`, each corresponding to a JSON file in the directory.
             If no files are found or an error occurs, it may return None or an incomplete list.
+
+        Example:
+            Using a middleware lambda function to modify an attribute of the mapped objects:
+
+            ```python
+            albums: List[AlbumModel] = parser.parse_all(
+                middleware=lambda mapped_object, json_data: (
+                    setattr(mapped_object, 'attribute_name', 'value') or mapped_object
+                )
+            )
+            ```
+
+            This middleware function sets the 'attribute_name' of each mapped_object to 'value'
+            and ensures the modified object is returned.
         """
         if not os.path.exists(self.__items_folder_path):
             print(f"No directory was found at {self.__items_folder_path}")
@@ -85,6 +108,11 @@ class SpotifyParser:
             file_path = os.path.join(self.__items_folder_path, file)
             mapped_object = self.parse_single(file_path)
             if mapped_object:
+                if middleware:
+                    # Pass the mapped object and the original JSON data to the middleware function
+                    with open(file_path, 'r') as f:
+                        json_data = json.load(f)
+                    mapped_object = middleware(mapped_object, json_data)
                 object_list.append(mapped_object)
         return object_list
 
